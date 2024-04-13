f942608b7cb73130f540baf2b525fdb3470d1575
Sergey <sergeyt.devwizard@gmail.com>
2024-04-13 13:28:13 +0300

    init

---
+++ src/components/layouts/showcaseLayouts/ShowcaseFooter/ShowcaseFooter.tsx
@@ -0,0 +1,7 @@
+import classes from './ShowcaseFooter.module.css';
+
+const ShowcaseFooter: React.FC = () => {
+  return <footer className={classes.footer}>375 SHOP</footer>;
+};
+
+export default ShowcaseFooter;
